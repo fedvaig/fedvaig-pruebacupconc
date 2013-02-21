6915815e92996f37f59e85c5b2d9612b0aa97bc1
Federico <Federico@Federico-PC>
2013-02-20 22:57:22 -0300

    Esta es la primera carga

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Federico
+ *
+ * Created on 20 de febrero de 2013, 22:24
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
